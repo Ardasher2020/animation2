bf9e852a2815945a5112f6ee461113de0bdf5383
Ardasher2020 <ardaserolimov4@gmail.com>
2024-12-15 12:30:35 +0500

    cretad new pages

---
+++ app.js
@@ -0,0 +1,26 @@
+ let text="welcome my"
+ console.log(text);
+ let text2="1"
+ console.log(text2)
+ let text3="2"
+ console.log(text3)
+ let text4="3"
+ console.log(text4)
+ let text5="5"
+ console.log(text5)
+ let text6="6"
+ console.log(text6)
+ let text7="7"
+ console.log(text7)
+ let text8="8"
+ console.log(text8)
+ let text9="9"
+ console.log(text9)
+ let text10="10"
+ console.log(text10)
+ let text11="My name is Ardasher"
+ console.log(text11)
+ let text12="i am 14 years old"
+ console.log(text12)
+ let text13="I live in Irkutsk"
+ console.log(text13)
