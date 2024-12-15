92ff886015fe51a8353deb7475cdd0f6bfeb32d6
Ardasher2020 <ardaserolimov4@gmail.com>
2024-12-15 15:39:54 +0500

    cretad new pages

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
- let text="welcome my"
- console.log(text);
- let text2="1"
- console.log(text2)
- let text3="2"
- console.log(text3)
- let text4="3"
- console.log(text4)
- let text5="5"
- console.log(text5)
- let text6="6"
- console.log(text6)
- let text7="7"
- console.log(text7)
- let text8="8"
- console.log(text8)
- let text9="9"
- console.log(text9)
- let text10="10"
- console.log(text10)
- let text11="My name is Ardasher"
- console.log(text11)
- let text12="i am 14 years old"
- console.log(text12)
- let text13="I live in Irkutsk"
- console.log(text13)
+let sum1 =85;
+let sum2 =45;
+console.log(sum2 + sum2);
+let sum3 =99;
+let sum4 =15;
+let sum5 =59;
+console.log(sum3+sum4+sum5);
+console.log("результат:",sum2+sum2);
+alert("привет всем")
+let result = sum2+sum2 
+console.log("результат:",result);
